a25c9d5a7ad211ded4265a389f331a33e9e6572e
Kyohei <k1229bmw@gmail.com>
2021-06-10 22:57:05 +0900

    commit2

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
-import '../styles/global.css'
+import 'tailwindcss/tailwind.css'
 import { AppProps } from 'next/app'
 import React from 'react'
 
-export default function App({ Component, pageProps }: AppProps) {
+
+function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
+
+export default MyApp
+//export default function App({ Component, pageProps }: AppProps) {
+  //return <Component {...pageProps} />
+//}
 
