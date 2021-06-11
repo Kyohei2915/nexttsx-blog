1fd79d5a3927bb0e888dd98297d333972292bb18
Kyohei <k1229bmw@gmail.com>
2021-06-11 09:03:45 +0900

    commit

---
+++ components/Header.tsx
@@ -0,0 +1,19 @@
+import Link from 'next/link';
+import ThemeSwitch from './theme-switch';
+
+
+const Header: React.FC = () => {
+  return (
+    <header className="py-2">
+      <div className="flex justify-between items-center">
+        <Link href="/">
+          <a className="text-2xl font-bold text-green-500">{'Kyohei Test'}</a>
+        </Link>
+
+        <ThemeSwitch />
+      </div>
+    </header>
+  );
+};
+
+export default Header;
