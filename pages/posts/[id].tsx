68f7ace6fa8a1a5154e454ffb80311c86270215a
Kyohei <k1229bmw@gmail.com>
2021-06-11 16:51:05 +0900

    boomit

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -62,7 +62,7 @@ const PostPage: React.FC<Props> = ({ source, frontMatter }: Props) => {
 export default PostPage;
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
-  const { content, data } = getPost(params?.slug as string);
+  const { content, data } = getPost(params?.id as string);
 
   const mdxSource = await serialize(content, { scope: data });
 
@@ -75,11 +75,11 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
 };
 
 export const getStaticPaths: GetStaticPaths = async () => {
-  const posts = getAllPosts(['slug']);
+  const posts = getAllPosts(['id']);
 
   const paths = posts.map((post) => ({
     params: {
-      slug: post.slug,
+      id: post.id,
     },
   }));
 
