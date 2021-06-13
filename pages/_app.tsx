63fe0bc7b1490b465276d88da4694680545b6e05
Kyohei <k1229bmw@gmail.com>
2021-06-13 16:17:42 +0900

    commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,21 @@
-//import { ThemeProvider } from 'next-themes';
 import '../styles/global.css';
 import Layout from '../components/layout';
 import Head from 'next/head';
 import type { AppProps } from 'next/app';
 import Footer from '../components/footer';
 import Header from '../components/Header';
+import { ThemeProvider } from 'next-themes';
+import 'tailwindcss/tailwind.css';
+
+
 
 function App({ Component, pageProps }: AppProps) {
   return (
     <><Header /><Layout>
       <Head>
+      <ThemeProvider attribute="class">
+      <Component {...pageProps} />
+      </ThemeProvider>
         <title>Next.js Starter Tailwind</title>
         <meta
           name="Description"
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  darkMode: 'class',
   purge: [
     // Use *.tsx if using TypeScript
     './pages/**/*.tsx',
