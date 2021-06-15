78f4049b5d10e9e457bd55bbda1791e0f10c9f96
Kyohei <k1229bmw@gmail.com>
2021-06-15 18:25:44 +0900

    commit

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,7 +6,7 @@ const Header: React.FC = () => {
     <header className="py-2">
       <div className="flex justify-between items-center">
           <Link href="/">
-            <h1 className="text-lg md:text-xl font-bold ml-3 text-gray-900 dark:text-white">
+            <h1 className="text-xl md:text-xl font-bold ml-3 text-gray-900 dark:text-white">
               Kyohei Blog
             </h1>
           </Link>
